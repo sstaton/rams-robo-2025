51c3f4efb1e8f034f2f17e19b4bfa424a2e47791
Emma <emmatowns@henrycountyboe.org>
2025-11-07 17:15:55 -0600

    626

--- main.py
+++ main.py
@@ -623,6 +623,10 @@ def opcontrol():
             pneum1.set(False)
 
         intake.spin(FORWARD, (btn_r1() - btn_r2()) * 100, PERCENT)
+        if button x:
+            spin_guide(FORWARD, 100, PERCENT)
+        else:
+            stop_guide()
         wait(20, MSEC)
 
 
