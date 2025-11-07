b2fe34d441883320320a5d1caf28cded5168b2aa
Emma <emmatowns@henrycountyboe.org>
2025-11-07 17:50:38 -0600

    fixed elif

--- main.py
+++ main.py
@@ -625,7 +625,7 @@ def opcontrol():
         intake.spin(FORWARD, (btn_r1() - btn_r2()) * 100, PERCENT)
         if btn_x():
             spin_guide(100, PERCENT)
-            elif btn_b():
+        elif btn_b():
             spin_guide(-100, PERCENT)
         else:
             stop_guide()
