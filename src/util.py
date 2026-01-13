1f939e8ee052788ebc0e22762189f35c041b1798
Iamverycreative <Charlesarndt@henrycountyboe.org>
2026-01-12 19:13:52 -0600

    We still moomin

--- src/util.py
+++ src/util.py
@@ -60,7 +60,7 @@ def within_range(value, base_value, range):
 # SHORTHAND
 # global DRIVE_REV_TO_IN
 DRIVE_REV_TO_IN = SMALL_OMNI_CIRC * (36.0/48.0)
-# 6.47953484803
+# 6.47953484803 X
 
 def pos_drive_r():
     return drive_r.position(REV) * DRIVE_REV_TO_IN
