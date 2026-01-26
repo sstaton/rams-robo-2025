b4829a3d9458a71fdf240d9075b08648213ed0e8
dogtag32 <dogtag220055@gmail.com>
2026-01-26 13:17:00 -0600

    TRISTANS CODE

--- main.py
+++ main.py
@@ -769,15 +769,15 @@ preauton()
 do_testing = False
 
 team_color = "RED"
-field_side = "LEFT"
+field_side = "RIGHT"
 
 def auton_function():    
-    if team_color == "RED" and field_side == "LEFT":
-        auton_function = autonomous_RED_LEFT
+    if team_color == "RED" and field_side == "RIGHT":
+        auton_function = autonomous_RED_RIGHT
         global GOOD_COLOR, BAD_COLOR
         GOOD_COLOR = Color.RED
         BAD_COLOR = Color.BLUE
-        return autonomous_RED_LEFT
+        return autonomous_RED_RIGHT
 if do_testing:
     print("do nothing")
 else:
