78faea95654abb12c0635bc9e0191b7252979715
Iamverycreative <Charlesarndt@henrycountyboe.org>
2026-02-16 19:16:59 -0600

    Code

--- src/auton.py
+++ src/auton.py
@@ -16,34 +16,37 @@ def autonomous():
     drive1.set_drive_velocity(300, RPM)
     drive1.set_turn_velocity(300, RPM)
     unloader.set(True)
+    descorer.set(False)
 
 
     # intake.spin(REVERSE)
     # outtake2.spin(FORWARD)
     drive_straight(-46, 68, 50)
     # wait(200, MSEC)
-    drive_turn(-80, 5.5, 50, 50, False)
+    drive_turn(-80, 5.5, 20, 30, False)
     # wait(200, MSEC)
-    drive_straight(-12, 30, 30)
+
+    drive_straight(-12, 10, 20) # slowed speed down some so we don't hit wall so hard
+
     intake.spin(REVERSE)
     splitter.set(True)
     start_time = time.time()
     time_now = time.time()
-    while(time_now < start_time + 4):
-        brain.screen.clear_row(6)
-        brain.screen.set_cursor(6, 1)
+    while(time_now < start_time + 1):
+        # brain.screen.clear_row(6)
+        # brain.screen.set_cursor(6, 1)
         brain.screen.print("Loop started")
         found_color1 = findcolor1()
         found_color2 = findcolor2()
-        brain.screen.clear_row(7)
-        brain.screen.set_cursor(7, 1)
-        brain.screen.print(found_color1)
-        brain.screen.clear_row(8)
-        brain.screen.set_cursor(8, 1)
-        brain.screen.print(found_color2)
-        brain.screen.clear_row(9)
-        brain.screen.set_cursor(9, 1)
-        brain.screen.print("Looking for", Color.BLUE)
+        # brain.screen.clear_row(7)
+        # brain.screen.set_cursor(7, 1)
+        # brain.screen.print(found_color1)
+        # brain.screen.clear_row(8)
+        # brain.screen.set_cursor(8, 1)
+        # brain.screen.print(found_color2)
+        # brain.screen.clear_row(9)
+        # brain.screen.set_cursor(9, 1)
+        # brain.screen.print("Looking for", Color.BLUE)
         if found_color1 == Color.BLUE and found_color2 == Color.BLUE:
             found_colora = "Blue"
         if found_colora == "Blue":
@@ -56,9 +59,21 @@ def autonomous():
         # drive_straight(-2, 4, 4)
         wait(10, MSEC)
         time_now = time.time()
-    drive_turn(-1, 5.5, 10, 20, False)
-    drive_straight(48, 68, 50)
+    drive_straight(2, 4, 4)
+    drive_turn(-5, 5.5, 3, 10, False)
+    drive_straight(43, 68, 50)
     outtake2.spin(FORWARD)
+    descorer.set(True)
+
+    wait(2100, MSEC) # let the balls out for a couple secs
+    drive_r.spin_for(FORWARD, 990, DEGREES) # only spin the right side so we make a wide arc
+
+    # line up
+    drive_straight(29, 25, 40)
+    drive_turn(-84, 5.5, 10, 20, False)
+
+    drive_straight(32, 26, 50) # push em to the middle
+
     # drive_straight(20, 40, 40)
     # drive_turn(-90, 5.5, 40, 30, False)
     # drive_straight(10, 20, 10)
